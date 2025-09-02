5c7e951a656c526d1e49c5d5f63a384520f52d9d
Samuel Sexton <sexton.s67357@gmail.com>
2025-09-01 22:39:08 -0500

    Initial streamlit app setup

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("MTG card search with symantic search support.")
+
+st.html("<h2>Coming Soon:<h2>")
+st.write("""Search function with all the regular searches that 
+         you are familiar with along with symatic search support.""")
